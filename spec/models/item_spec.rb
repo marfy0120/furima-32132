cc0fbfd87c547a487aea9b45951689cce5a6d572
marfy0120 <marfy0528@icloud.com>
2020-12-07 21:46:11 +0900

    テストコード実装

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,81 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+
+  before do
+    @item = FactoryBot.build(:item)
+  end
+  it "imageが空だと登録できない" do
+    
+    @item.image = ""  
+    @item.valid?
+    expect(@item.errors.full_messages).to include "User must exist"
+  end
+
+  it "product_nameが空だと登録できない" do
+    
+    @item.product_name = ""  
+    @item.valid?
+    expect(@item.errors.full_messages).to include "User must exist"
+  end
+
+  it "priceが空だと登録できない" do
+    
+    @item.price = ""  
+    @item.valid?
+    expect(@item.errors.full_messages).to include "User must exist"
+  end
+
+  it "descriptionが空だと登録できない" do
+    
+    @item.description = ""  
+    @item.valid?
+    expect(@item.errors.full_messages).to include "User must exist"
+  end
+
+  it "cost_idが空だと登録できない" do
+    
+    @item.cost_id = ""  
+    @item.valid?
+    expect(@item.errors.full_messages).to include "User must exist"
+  end
+
+  it "status_idが空だと登録できない" do
+    
+    @item.status_id = ""  
+    @item.valid?
+    expect(@item.errors.full_messages).to include "User must exist"
+  end
+
+  it "day_idが空だと登録できない" do
+    
+    @item.day_id = ""  
+    @item.valid?
+    expect(@item.errors.full_messages).to include "User must exist"
+    end
+
+    it "prefecture_idが空だと登録できない" do
+    
+      @item.prefecture_id = ""  
+      @item.valid?
+      expect(@item.errors.full_messages).to include "User must exist"
+      end
+
+
+      it "category_idが空だと登録できない" do
+    
+        @item.category_id = ""  
+        @item.valid?
+        expect(@item.errors.full_messages).to include "User must exist"
+        end
+
+        it "category_idが空だと登録できない" do
+    
+          @item.category_id = ""  
+          @item.valid?
+          expect(@item.errors.full_messages).to include "User must exist"
+          end
+  
+  
 end
