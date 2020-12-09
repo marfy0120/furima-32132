3463b9f26d88b254058e0238f98d33e6d89441c8
marfy0120 <marfy0528@icloud.com>
2020-12-09 20:01:42 +0900

    編集機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show, :edit]
+  before_action :move_to_index, only: [:edit]
   def index
     @items = Item.all.order(created_at: :desc)
     
@@ -31,16 +32,24 @@ class ItemsController < ApplicationController
 
   def update
     @item = Item.find(params[:id])
-    if @item.save
-      @item.update(item_params)
-      redirect_to item_path
-      else
-        render :show
-    end
+    @item.update(item_params)
+    return redirect_to item_path if @item.valid?
+    # # if 
+    #   redirect_to item_path
+      # else
+    render :edit
+    # end
   end
+
 
   private
   def item_params
     params.require(:item).permit(:product_name, :price, :description, :category_id, :prefecture_id, :image, :status_id, :cost_id, :day_id).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to root_path
+    end
   end
 end
