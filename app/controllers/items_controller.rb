96cc3a18aa13b254cfc66b13f275646a1fb69211
marfy0120 <marfy0528@icloud.com>
2020-12-10 10:51:57 +0900

    変更点編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -41,11 +41,18 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
-    redirect_to root_path
-    
+    @item = Item.find(params[:id])
+    if @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
+    # item = Item.find(params[:id])
+    # item.destroy
+    # redirect_to root_path
+    
+  
 
 
   private
